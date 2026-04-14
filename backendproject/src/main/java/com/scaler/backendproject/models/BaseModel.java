b2d340bfb7750272915d7c12cf82e79f5dde289a
asher <asher33666@gmail.com>
2026-04-14 22:18:52 +0530

    added BaseModel class for common entity fields

---
+++ backendproject/src/main/java/com/scaler/backendproject/models/BaseModel.java
@@ -0,0 +1,12 @@
+package com.scaler.backendproject.models;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+public class BaseModel {
+}
