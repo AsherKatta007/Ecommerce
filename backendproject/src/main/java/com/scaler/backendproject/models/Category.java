4026669baea07196a597a46dcb58f0cc114c6409
asher <asher33666@gmail.com>
2026-04-14 22:23:58 +0530

    added Category entity with JPA and JSON annotations

---
+++ backendproject/src/main/java/com/scaler/backendproject/models/Category.java
@@ -0,0 +1,12 @@
+package com.scaler.backendproject.models;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.List;
+public class Category {
+}
