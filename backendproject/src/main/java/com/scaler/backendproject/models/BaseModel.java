c3c1c2c7b2bfe180376976100154dff0641bc8cb
asher <asher33666@gmail.com>
2026-04-15 22:22:21 +0530

    Create BaseModel with id, timestamps, soft delete flag, and createdBy field

--- backendproject/src/main/java/com/scaler/backendproject/models/BaseModel.java
+++ backendproject/src/main/java/com/scaler/backendproject/models/BaseModel.java
@@ -1,12 +1,36 @@
 package com.scaler.backendproject.models;
-import jakarta.persistence.Entity;
+
 import jakarta.persistence.EntityListeners;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
+
+
 public class BaseModel {
+
+
+    private Long id;
+
+
+    private LocalDateTime createdAt;
+
+
+    private LocalDateTime updatedAt;
+
+    private boolean isDeleted;
+
+    private String createdBy;
 }
+
