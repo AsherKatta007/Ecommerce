5d0305e9f0c8c0fe6d5b807540d46ab8253bd051
asher <asher33666@gmail.com>
2026-04-16 20:28:38 +0530

    implement Category entity with OneToMany mapping
    - Add title field
    - Add OneToMany relationship with Product
    - Configure lazy loading and cascade
    - Add auditing support with EntityListeners
    - Override toString method

--- backendproject/src/main/java/com/scaler/backendproject/models/BaseModel.java
+++ backendproject/src/main/java/com/scaler/backendproject/models/BaseModel.java
@@ -1,38 +1,39 @@
 package com.scaler.backendproject.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity
-@EntityListeners(AuditingEntityListener.class)
-public class Product extends BaseModel {
-
-    private String title;
-    private String description;
-    private Double price;
-    private String imageUrl;
-
-    @ManyToOne
-    private Category category;
-
-    @Override
-    public String toString() {
-        return "Product{" +
-                "title='" + title + '\'' +
-                ", description='" + description + '\'' +
-                ", price=" + price +
-                ", imageUrl='" + imageUrl + '\'' +
-                ", category=" + category +
-                '}';
-    }
+@MappedSuperclass
+public class BaseModel implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @CreatedDate
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+    private boolean isDeleted;
+    //This (createdBy) was optional in the class
+    @CreatedBy
+    private String createdBy;
 }
