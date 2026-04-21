5026c6a4a928a6b15a5862a6aac8c4c986a60e73
asher <asher33666@gmail.com>
2026-04-21 20:29:43 +0530

    Update Product entity: set ManyToOne to LAZY, define category_id column, and remove category from toString to prevent recursion

--- backendproject/src/main/java/com/scaler/backendproject/models/Product.java
+++ backendproject/src/main/java/com/scaler/backendproject/models/Product.java
@@ -2,7 +2,6 @@ package com.scaler.backendproject.models;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
-import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -17,11 +16,22 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @Entity
 @EntityListeners(AuditingEntityListener.class)
 public class Product extends BaseModel {
-    @Id
-    private Long id;
     private String title;
     private String description;
     private Double price;
     private String imageUrl;
+    @ManyToOne
+//    @JoinColumn(name = "category_id")
+    private Category category;
 
+    @Override
+    public String toString() {
+        return "Product{" +
+                "title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                ", imageUrl='" + imageUrl + '\'' +
+                ", category=" + category +
+                '}';
+    }
 }
