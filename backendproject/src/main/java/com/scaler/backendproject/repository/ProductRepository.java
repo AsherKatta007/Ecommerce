634421f6ba23d552c41a5f9cab7f4376287b0551
asher <asher33666@gmail.com>
2026-04-22 22:10:43 +0530

    create ProductRepository with required imports and base structure

--- backendproject/src/main/java/com/scaler/backendproject/repository/ProductRepository.java
+++ backendproject/src/main/java/com/scaler/backendproject/repository/ProductRepository.java
@@ -1,4 +1,13 @@
 package com.scaler.backendproject.repository;
 
+import com.scaler.backendproject.models.Product;
+import com.scaler.backendproject.repository.projections.ProductProjection;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 public class ProductRepository {
 }
