8fbd8df869e3a846f524061deed5e3a901ad4206
asher <asher33666@gmail.com>
2026-04-22 22:21:29 +0530

    implement ProductRepository with JPA query methods

--- backendproject/src/main/java/com/scaler/backendproject/repository/ProductRepository.java
+++ backendproject/src/main/java/com/scaler/backendproject/repository/ProductRepository.java
@@ -9,5 +9,26 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-public class ProductRepository {
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    //Hibernate will create a query for us automatically, behind the scenes
+    //And we do not have to write a query like below:
+
+    //This will insert product records in my product table
+    Product save(Product product);
+
+    //This will return the list of all products
+    Page<Product> findAll(Pageable pageable);
+
+    //SELECT * FROM Product WHERE description = xyz (passed argument)
+    Product findByDescription(String description);
+
+    //SELECT * FROM Product WHERE title = xyz (passed argument)
+    Product findByTitle(String title);
+
+    //SELECT * FROM Product WHERE description = xyz AND title = abc (passed arguments)
+    Product findByTitleAndDescription(String title, String description);
+
+
 }
