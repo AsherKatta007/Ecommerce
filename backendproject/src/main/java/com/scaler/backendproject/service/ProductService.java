d6b7eec4572d607b701660b5d0201c2327df5655
asher <asher33666@gmail.com>
2026-07-26 00:16:01 +0530

    feat: define ProductService interface with CRUD operations

--- backendproject/src/main/java/com/scaler/backendproject/service/ProductService.java
+++ backendproject/src/main/java/com/scaler/backendproject/service/ProductService.java
@@ -6,5 +6,11 @@ import com.scaler.backendproject.models.Category;
 import com.scaler.backendproject.models.Product;
 import org.springframework.data.domain.Page;
 
-public class ProductService {
+public interface ProductService {
+    Product getSingleProduct(Long id) throws ProductNotFoundException;
+    Page<Product> getAllProducts(int pageNumber, int pageSize, String fieldName) throws ProductNotFoundException;
+    Product createProduct(Long id, String title, String description, Double price, String category, String imageUrl);
+    Product deleteProduct(Long id) throws ProductNotFoundException;
+    Product updateProduct(Long id, String title, String description, Double price, Category category, String imageUrl) throws ProductNotFoundException;
+    //Product[] getAllProducts();
 }
