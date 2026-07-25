3f3f1905bdce131aa6cc882f914489068bbf63a0
asher <asher33666@gmail.com>
2026-07-25 12:19:00 +0530

    feat: add CategoryRepository with custom query method

--- backendproject/src/main/java/com/scaler/backendproject/repository/CategoryRepository.java
+++ backendproject/src/main/java/com/scaler/backendproject/repository/CategoryRepository.java
@@ -1,4 +1,12 @@
 package com.scaler.backendproject.repository;
 
-public class CategoryRepository {
+
+import com.scaler.backendproject.models.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    //    Category findById(long id);
+    Optional<Category> findByTitle(String title);
 }
