cfdbbf97a5c79cf20c069a2d70aa85b3ce6ed3b8
asher <asher33666@gmail.com>
2026-07-26 00:05:08 +0530

    chore: add required imports to ProductService

--- backendproject/src/main/java/com/scaler/backendproject/service/ProductService.java
+++ backendproject/src/main/java/com/scaler/backendproject/service/ProductService.java
@@ -1,4 +1,10 @@
 package com.scaler.backendproject.service;
 
+
+import com.scaler.backendproject.exceptions.ProductNotFoundException;
+import com.scaler.backendproject.models.Category;
+import com.scaler.backendproject.models.Product;
+import org.springframework.data.domain.Page;
+
 public class ProductService {
 }
