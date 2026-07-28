11965b7b3fd0393f4fd137697002099fb6dcea08
asher <asher33666@gmail.com>
2026-07-28 16:39:11 +0530

    feat: initialize SelfProductService

--- backendproject/src/main/java/com/scaler/backendproject/service/SelfProductService.java
+++ backendproject/src/main/java/com/scaler/backendproject/service/SelfProductService.java
@@ -1,4 +1,17 @@
 package com.scaler.backendproject.service;
 
+import com.scaler.backendproject.exceptions.ProductNotFoundException;
+import com.scaler.backendproject.models.Category;
+import com.scaler.backendproject.models.Product;
+import com.scaler.backendproject.repository.CategoryRepository;
+import com.scaler.backendproject.repository.ProductRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service("selfProductService")
 public class SelfProductService {
 }
