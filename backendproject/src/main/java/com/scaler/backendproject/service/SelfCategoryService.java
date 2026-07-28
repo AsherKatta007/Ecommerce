8c15728de824debebd4908b321dda458fa3cd8ba
asher <asher33666@gmail.com>
2026-07-28 16:29:12 +0530

    feat: implement category service with CRUD operations

--- backendproject/src/main/java/com/scaler/backendproject/service/SelfCategoryService.java
+++ backendproject/src/main/java/com/scaler/backendproject/service/SelfCategoryService.java
@@ -20,3 +20,35 @@ public class SelfCategoryService implements CategoryService {
         this.productRepository = productRepository;
         this.categoryRepository = categoryRepository;
     }
+
+    @Override
+    public Category createCategory(String categoryTitle) {
+        Category category = new Category();
+        category.setTitle(categoryTitle);
+
+        //Saving the Category in the repository as well as returning it in the same command.
+        return categoryRepository.save(category);
+    }
+
+    @Override
+    public List<Product> getProductsInCategory(String categoryTitle) throws NoSuchElementException {
+        Optional<Category> category;
+        Optional<List<Product>> productsInCategory;
+        try {
+            category = categoryRepository.findByTitle(categoryTitle);
+            productsInCategory = Optional.of(productRepository.getProductByCategoryId(category.get().getId()));
+        } catch (NoSuchElementException e) {
+            throw new NoSuchElementException("No products found under " + categoryTitle);
+        }
+        return productsInCategory.get();
+    }
+
+    @Override
+    public List<Category> getAllCategories() throws NullPointerException {
+        Optional<List<Category>> listOfCategories = Optional.of(categoryRepository.findAll());
+        if (listOfCategories.isEmpty()) {
+            throw new NullPointerException("No Categories in the database");
+        }
+        return listOfCategories.get();
+    }
+}
