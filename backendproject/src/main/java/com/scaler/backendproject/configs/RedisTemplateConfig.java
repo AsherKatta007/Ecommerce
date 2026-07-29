a2b15ee0954fbd4bc8fd964dcd133ba6a8439cc2
asher <asher33666@gmail.com>
2026-07-29 15:09:02 +0530

    feat: configure RedisTemplate bean

--- backendproject/src/main/java/com/scaler/backendproject/configs/RedisTemplateConfig.java
+++ backendproject/src/main/java/com/scaler/backendproject/configs/RedisTemplateConfig.java
@@ -1,4 +1,13 @@
 package com.scaler.backendproject.configs;
 
+import com.scaler.backendproject.models.Product;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
 public class RedisTemplateConfig {
 }
