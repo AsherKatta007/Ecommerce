277feeb78b21c575874b62262a740ebd74653bfe
asher <asher33666@gmail.com>
2026-07-29 15:46:01 +0530

    feat(product): add Redis caching for product retrieval and implement create product API

--- backendproject/src/main/java/com/scaler/backendproject/service/FakeStoreProductService.java
+++ backendproject/src/main/java/com/scaler/backendproject/service/FakeStoreProductService.java
@@ -17,4 +17,98 @@ import org.springframework.web.client.RestTemplate;
 //so create an object of this
 @Service("fakeStoreProductService")
 public class FakeStoreProductService {
+
+    //Inside this, fake store is going to be third party service
+
+    //    @Autowired
+    private final RestTemplate restTemplate;
+    //    @Autowired
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    public FakeStoreProductService(RestTemplate restTemplate, RedisTemplate<String, Object> redisTemplate) {
+        this.restTemplate = restTemplate;
+        this.redisTemplate = redisTemplate;
+    }
+
+    @Override
+    public Product getSingleProduct(Long id) throws ProductNotFoundException {
+        System.out.println("We are inside the single product in FakeStoreProductService");
+
+        //First part assume it as table name
+        //Second part: key of the product
+        Product redisProduct = (Product) redisTemplate
+                .opsForHash()
+                .get("PRODUCTS", "PRODUCTS_" + id);
+
+        if (redisProduct != null) {
+            //Cache Hit
+            return redisProduct;
+        }
+
+        //If the earlier 'if' condition is not true
+        //Then we have cache miss, and then we shall hit the api for FakeStore
+
+        FakeStoreProductDTO fakeStoreProductDTO =
+                restTemplate.getForObject("https://fakestoreapi.com/products/" + id,
+                        FakeStoreProductDTO.class);
+
+//        System.out.println(fakeStoreProductDTO.toString());
+        if (fakeStoreProductDTO == null) {
+            throw new ProductNotFoundException("Product Not Found with id: " + id);
+        }
+
+        //Because we had cache miss, now we shall do cache eviction
+        redisTemplate.opsForHash().put("PRODUCTS", "PRODUCTS_" + id, fakeStoreProductDTO.getProduct());
+
+        return fakeStoreProductDTO.getProduct();
+    }
+
+
+//    @Override
+//    public Product[] getAllProducts(FakeStoreProductDTO[] listOfProducts) {
+//        return new Product[0];
+//    }
+
+//    public List<Product> getAllProducts() throws ProductNotFoundException {
+//        System.out.println("In the getAllProducts API in FKSPS");
+//        FakeStoreProductDTO[] fakeStoreListOfProducts =
+//                restTemplate.getForObject("https://fakestoreapi.com/products/",
+//                        FakeStoreProductDTO[].class);
+//        if (fakeStoreListOfProducts == null) {
+//            throw new ProductNotFoundException("No Products Found in the Database");
+//        }
+//        return new FakeStoreProductDTO().getListOfProducts(fakeStoreListOfProducts);
+//    }
+
+    @Override
+    public Page<Product> getAllProducts(int pageNumber, int pageSize, String fieldName) throws ProductNotFoundException {
+        FakeStoreProductDTO[] fakeStoreListOfProducts =
+                restTemplate.getForObject("https://fakestoreapi.com/products/",
+                        FakeStoreProductDTO[].class);
+        if (fakeStoreListOfProducts == null) {
+            throw new ProductNotFoundException("No Products Found in the Database");
+        }
+        return (Page<Product>) new FakeStoreProductDTO().getListOfProducts(fakeStoreListOfProducts);
+    }
+
+    @Override
+    public Product createProduct(Long id, String title, String description,
+                                 Double price, String category, String imageUrl) {
+        FakeStoreProductDTO fakeStoreProductDTO = new FakeStoreProductDTO();
+        fakeStoreProductDTO.setId(id);
+        fakeStoreProductDTO.setTitle(title);
+        fakeStoreProductDTO.setDescription(description);
+        fakeStoreProductDTO.setPrice(price);
+        fakeStoreProductDTO.setCategory(category);
+        //Added this as Homework
+        //So we had to change the FakeStoreProductService class
+        //And since FakeStoreProductService implements ProductService Interface
+        //We had to change that method signature as well
+        fakeStoreProductDTO.setImage(imageUrl);
+
+        FakeStoreProductDTO response = restTemplate.postForObject("https://fakestoreapi.com/products",
+                fakeStoreProductDTO, FakeStoreProductDTO.class);
+
+        return response.getProduct();
+    }
 }
