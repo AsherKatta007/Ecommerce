c0ae4f5a5d828b21debb43c5c1990c40af0ce456
asher <asher33666@gmail.com>
2026-07-29 18:18:29 +0530

    feat(payment): initialize Stripe payment gateway integration

--- backendproject/src/main/java/com/scaler/backendproject/service/StripePaymentGatewayImplementation.java
+++ backendproject/src/main/java/com/scaler/backendproject/service/StripePaymentGatewayImplementation.java
@@ -1,4 +1,13 @@
 package com.scaler.backendproject.service;
 
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentLink;
+import com.stripe.model.Price;
+import com.stripe.param.PaymentLinkCreateParams;
+import com.stripe.param.PriceCreateParams;
+import org.springframework.stereotype.Service;
+
+@Service
 public class StripePaymentGatewayImplementation {
 }
