184910e0c4858d4c9d273ce3af8a39ff01de2655
asher <asher33666@gmail.com>
2026-07-29 15:01:13 +0530

    feat: initialize FakeStoreProductService

--- backendproject/src/main/java/com/scaler/backendproject/service/FakeStoreProductService.java
+++ backendproject/src/main/java/com/scaler/backendproject/service/FakeStoreProductService.java
@@ -1,4 +1,20 @@
 package com.scaler.backendproject.service;
 
+import com.scaler.backendproject.dto.FakeStoreProductDTO;
+import com.scaler.backendproject.exceptions.ProductNotFoundException;
+import com.scaler.backendproject.models.Category;
+import com.scaler.backendproject.models.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+//This annotation here is going to tell SpringBoot
+//that this is one of our important class
+//so create an object of this
+@Service("fakeStoreProductService")
 public class FakeStoreProductService {
 }
