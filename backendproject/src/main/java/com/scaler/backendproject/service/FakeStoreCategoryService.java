69ff54bb42e62e467ac8a3b54aa6eb51ef44de89
asher <asher33666@gmail.com>
2026-07-29 14:38:10 +0530

    feat: initialize FakeStoreCategoryService

--- backendproject/src/main/java/com/scaler/backendproject/service/FakeStoreCategoryService.java
+++ backendproject/src/main/java/com/scaler/backendproject/service/FakeStoreCategoryService.java
@@ -1,4 +1,17 @@
 package com.scaler.backendproject.service;
 
-public class FakeStoreCategoryService {
+import com.scaler.backendproject.dto.FakeStoreProductDTO;
+import com.scaler.backendproject.exceptions.CategoryNotFoundException;
+import com.scaler.backendproject.models.Category;
+import com.scaler.backendproject.models.Product;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@Service("fakeStoreCategoryService")
+public class FakeStoreCategoryService  implements CategoryService{
+
 }
