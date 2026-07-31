247754c58ae4ff893cb24134583a0dfab799eb50
asher <asher33666@gmail.com>
2026-07-31 16:15:38 +0530

    feat: implement CategoryNotFoundException

--- backendproject/src/main/java/com/scaler/backendproject/exceptions/CategoryNotFoundException.java
+++ backendproject/src/main/java/com/scaler/backendproject/exceptions/CategoryNotFoundException.java
@@ -1,4 +1,13 @@
 package com.scaler.backendproject.exceptions;
 
-public class CategoryNotFoundException {
+public class CategoryNotFoundException extends Exception {
+    //Create an object of CategoryNotFoundException Class
+    //And set the error method
+    public CategoryNotFoundException(String message) {
+        super(message);
+    }
+
+    public CategoryNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
